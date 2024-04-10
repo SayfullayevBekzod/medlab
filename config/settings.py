28c32827ad37e7aa19dd020b562b7a4a7a205b21
Bekzod <sayfulleyvbekzod465@gmail.com>
2024-04-10 22:03:17 +0500

    medlab

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
