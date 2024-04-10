12b09f311560b3b29ad959992535136d0ed313e0
Bekzod <sayfulleyvbekzod465@gmail.com>
2024-04-10 22:26:23 +0500

    medlab

--- config/urls.py
+++ config/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     # path('', include('admin_black.urls'))
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
